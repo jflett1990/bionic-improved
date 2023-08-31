1fade5b0ff095698b155d4ea7f47b58f1995fdda
jflett1990 <81728344+jflett1990@users.noreply.github.com>
2023-08-31 23:00:05 +0000

    fixed

--- untitled folder 5/content.js
+++ untitled folder 5/content.js
@@ -1,2 +1,23 @@
-  // Initial call
+ // content.js
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  function makeTextBionic(text) {
+
+    const words = text.split(' ');
+
+    return words.map((word) => {
+      //Modify text here
+    }).join(' ');
+
+  }
+
+  function processNode(node) {
+    //Process DOM nodes
+  }
+
   Array.from(document.body.childNodes).forEach(processNode);
+
+  console.log('Bionic Reading Script Loaded');
+
+});
